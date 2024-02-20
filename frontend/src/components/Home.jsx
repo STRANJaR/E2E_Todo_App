3af08988a37aa4945ddf9b6837524a7412980c0d
Rohit Shrivastav <rohitrks805@gmail.com>
2024-02-20 12:21:40 +0530

    added Outlet in react

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,96 +1,9 @@
-import { Link } from "react-router-dom"
-import axios from "axios";
-import { useState } from "react";
+
 
 function Home() {
-//   return (
-//     <div className="h-32 w-full ">
-//         <div className=" bg-slate-700 mx-80 rounded-full ">
-//             <ul className="flex justify-center ">
-//                 <Link 
-//                 to="/"
-//                 className="px-5 py-2 m-3 bg-violet-500  rounded-full  cursor-pointer">
-//                     Home</Link>
-
-//                 <Link
-//                 to="api/v1/user/register"
-//                 className="px-5 py-2 m-3 bg-violet-500   rounded-full cursor-pointer">
-//                     SignUp</Link>
-
-//                 <Link
-//                 to="api/v1/user/login"
-//                 className="px-5 py-2 m-3 bg-violet-500  rounded-full cursor-pointer">
-//                     LogIn</Link>
-//             </ul>
-//         </div>
-//     </div>
-//   )
-// }
-
-const [data, setData] = useState({
-    fullName: "",
-    email: "",
-    username: "",
-    password: "",
-  });
-
-  const handleChange = (e) =>{
-    const value = e.target.value;
-   setData({
-    ...data,
-    [e.target.name] : value
-   }
-   )
-  };
-
-  // const handleChange = (e) =>{
-  //   const value = e.target.value;
-  //   setData(...value)
-  // }
-
-  const handleSubmit = (e) =>{
-    e.preventDefault();
-    const userData = {
-      fullName: data.fullName,
-      email: data.email,
-      username: data.username,
-      password: data.password
-    };
-    axios.post("http://localhost:8000/api/v1/user/register", userData)
-    .then((response)=>{
-      console.log(response);
-    })
-    .catch((err)=>{
-        console.log("ERROR: ", err)
-    })
-  }
   return (
-    <div>
-    <h1>SignUp Account</h1>
-    <form onSubmit={handleSubmit}>
-        <label htmlFor="fullName">
-            fullName: 
-        <input className="text-black" type="text" name="fullName" id="fullName" defaultValue={data.fullName} onChange={handleChange} />
-        </label>
-        
-        <label htmlFor="email">
-            Email: 
-        <input className="text-black" type="email" name="email" id="email" defaultValue={data.email} onChange={handleChange} />
-        </label>
 
-        <label htmlFor="username">
-            Username: 
-        <input className="text-black" type="text" name="username" id="username" defaultValue={data.username} onChange={handleChange} />
-        </label>
-
-        <label htmlFor="password">
-            Password: 
-        <input className="text-black" type="password" name="password" id="password" defaultValue={data.password} onChange={handleChange} />
-        </label>
-    
-        <button className="bg-purple-500 p-2 rounded-sm" type="submit">Submit</button>
-    </form>
-    </div>
+    <h1>Home</h1>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Home from './components/Home.jsx'
 import Signup from './components/Signup.jsx'
 import Login from './components/Login.jsx'
+import Layout from './Layout.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home/>,
+    element: <Layout/>,
     children: [
       {
-        path: "api/v1/health",
+        path: "",
         element: <Home/>
       },
       {

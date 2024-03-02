e0008c9999713a0200853929e84bcdde2d6b73f7
Rohit Shrivastav <rohitrks805@gmail.com>
2024-03-02 23:12:28 +0530

    todo add api working

--- backend/routes/todo.routes.js
+++ backend/routes/todo.routes.js
@@ -10,8 +10,8 @@ import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = Router()
 
-router.route("/add-todo").post(verifyJWT, addTodo)
-router.route("/edit-todo/:todoId").patch(verifyJWT, editTodo)
+router.route("/add-todo").post( addTodo)
+router.route("/edit-todo/:todoId").patch( editTodo)
 router.route("/delete-todo/:todoId").delete( deleteTodo)
 router.route("/complete/:todoId").put(verifyJWT, isComplete)
 router.route("/all-todos/:userId").get( getAllTodos)
--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,8 +1,42 @@
-
+import axios from "axios"
+import { useState } from "react"
 
 function CreateTodo() {
+  const [title, setTitle] = useState("")
+
+  const handleTodo = async (e) => {
+    e.preventDefault()
+    try {
+      const { data } = await axios.post('http://localhost:8000/api/v1/todo/add-todo',
+      {
+        title
+      },
+      // {
+      //   headers: {
+      //     "Content-Type": "application/json"
+      //   },
+      //   withCredentials: true
+      // }
+      )
+      
+      console.log(data)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
-    <div>CreateTodo</div>
+    <>
+    <form onSubmit={handleTodo}>
+      <input 
+      type="text"
+      value={title}
+      onChange={((e)=>setTitle(e.target.value))}
+      placeholder="Enter todo"
+      />
+      <button type="submit">Add</button>
+    </form>
+    </>
   )
 }
 

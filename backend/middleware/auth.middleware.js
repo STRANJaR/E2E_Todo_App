790f135142a278941e2d349695692b3de0cf3328
Rohit Shrivastav <rohitrks805@gmail.com>
2024-03-05 20:56:49 +0530

    accessToken in header in JWT

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -5,7 +5,7 @@ import { ApiError } from "../utils/ApiError.js";
 
 export const verifyJWT = asyncHandler(async(req, res, next)=>{
     try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "acessToken")
 
         if(!token) throw new ApiError(401, "Unauthorized request")
 
--- backend/routes/todo.routes.js
+++ backend/routes/todo.routes.js
@@ -10,9 +10,9 @@ import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = Router()
 
-router.route("/add-todo").post( addTodo)
+router.route("/add-todo").post(verifyJWT, addTodo)
 router.route("/edit-todo/:todoId").patch( editTodo)
-router.route("/delete-todo/:todoId").delete( deleteTodo)
+router.route("/delete-todo/:todoId").delete(verifyJWT, deleteTodo)
 router.route("/complete/:todoId").put(verifyJWT, isComplete)
 router.route("/all-todos/:userId").get( getAllTodos)
 

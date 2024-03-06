bac667936aa94ee798356436630863de255b7051
Rohit Shrivastav <rohitrks805@gmail.com>
2024-03-06 10:12:12 +0530

    contact page all set

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,5 +1,8 @@
 import axios from 'axios'
 import { useState } from 'react'
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
 
 function Contact() {
   const [profile, setProfile] = useState({})
@@ -19,13 +22,13 @@ function Contact() {
   })
 
   return (
-    <section className="h-screen bg-blueShade">
+    <section className="h-screen bg-shadeGray">
     {/* <h1 className="text-whiteText text-3xl text-center">Hii</h1> */}
 
-    <div className='bg-shadeGray h-auto w-full text-whiteText'>
+    <div className=' h-auto w-full text-whiteText'>
       <div className=' px-40 py-10'>
         <img 
-        className='h-32'
+        className='h-32  rounded-full '
         src={profile.avatar_url} 
         alt="profile_img" />
 
@@ -39,20 +42,40 @@ function Contact() {
 
         <div>
           <p className='opacity-80 pt-10 pb-6'>For more : </p>
-          <button
+          {/* <button
           className='px-5  rounded-md py-3 bg-primaryColor hover:text-whiteText transition-all'
           
           >
            <a href={profile.html_url} target='_blank'>
+           <FaGithub />
+
             GitHub
+            
             </a></button>
 
           <button
-          className='px-5  rounded-md py-3 bg-blueShade hover:text-whiteText transition-all ml-5'
+          className='px-8   rounded-md py-3 bg-blueShade hover:text-whiteText transition-all ml-5'
           >
             <a href="https://www.linkedin.com/in/rohit-shrivastav-dev/" target='_blank'>
+            <FaLinkedin />
             LinkedIn
-              </a></button>
+
+              </a></button> */}
+              <div className='flex'>
+                <a 
+                className='px-8  text-[17px] flex justify-center items-center  rounded-full py-3 bg-[#2DBA4E] hover:bg-[#28A746] hover:text-whiteText transition-all ml-5'
+                href="#">
+            <FaGithub className='pr-1 text-2xl'/>
+                  
+                  GitHub</a>
+
+                <a 
+                className='px-8 text-[17px] flex justify-center items-center  rounded-full py-3 bg-[#0A66C2] hover:bg-[#0a66c2dc] hover:text-whiteText transition-all ml-5'
+                href="#">
+            <FaLinkedin  className='pr-1 text-2xl'/>
+                  
+                  LinkedIn</a>
+              </div>
         </div>
       </div>
     </div>

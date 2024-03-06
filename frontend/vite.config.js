d0b39a8c26147572e85a7fc895ae67c39c1ef228
Rohit Shrivastav <rohitrks805@gmail.com>
2024-03-06 10:13:34 +0530

    vite proxy setup CORS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api' : "http://localhost:8000/"
+      '/api' : "https://e2e-todo-app.vercel.app/"
     }
   }
 })
